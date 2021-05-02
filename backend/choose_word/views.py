8336d2ee822335e6150ff1290b323636c3398657
Mr.Geekman <bunin260200@mail.ru>
2021-05-02 23:42:21 +0300

    Solved bug with multitoken words

--- backend/choose_word/views.py
+++ backend/choose_word/views.py
@@ -1,8 +1,6 @@
 import numpy as np
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.parsers import JSONParser
-from rest_framework.exceptions import ValidationError
 from .serializers import TestItemSerializer
 from .apps import ChooseWordConfig
 
@@ -21,14 +19,11 @@ class ChooseWordBertView(APIView):
         mask_token = ChooseWordConfig.tokenizer.mask_token
         sentence = f'{sentence_left} {mask_token} {sentence_right}'
         candidates = data['candidates']
-        try:
-            scores = ChooseWordConfig.bert_scorer_correction(
-                [sentence], [candidates]
-            )[0]
-            # normalize scores
-            normalized_scores = np.exp(np.mean(scores, axis=1))
-            normalized_scores = normalized_scores / np.sum(normalized_scores)
-            # send predictions
-            return Response(data=normalized_scores)
-        except ValueError as e:
-            raise ValidationError(str(e))
+        scores = ChooseWordConfig.bert_scorer_correction(
+            [sentence], [candidates]
+        )[0]
+        # normalize scores
+        normalized_scores = np.exp([np.mean(x) for x in scores])
+        normalized_scores = normalized_scores / np.sum(normalized_scores)
+        # send predictions
+        return Response(data=normalized_scores)
