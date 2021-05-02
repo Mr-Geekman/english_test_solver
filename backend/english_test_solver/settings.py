b95e82b886575e85368b4097e395fcadc9930958
Mr.Geekman <bunin260200@mail.ru>
2021-05-02 19:23:58 +0300

    Updated settings

--- backend/english_test_solver/settings.py
+++ backend/english_test_solver/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!pcwa0sm$-+gg@3%20ib+i=6z4i-3v73%+un+zo0xi!w#r=5mi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
