843cc3ee5d00259dcf83e57ef0c488f77b0194a9
Mr.Geekman <bunin260200@mail.ru>
2021-05-13 13:51:49 +0300

    Fixed processing errors

--- backend/choose_word/urls.py
+++ backend/choose_word/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from .views import ChooseWordBertView
 
 
-handler500 = 'rest_framework.exceptions.server_error'
-handler400 = 'rest_framework.exceptions.bad_request'
-
 urlpatterns = [
     path('bert/', ChooseWordBertView.as_view()),
 ]
+
+handler500 = 'rest_framework.exceptions.server_error'
+handler400 = 'rest_framework.exceptions.bad_request'
