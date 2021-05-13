8f8ad921f34a36331e9c41377e2b4d15ca5e49ae
Mr.Geekman <bunin260200@mail.ru>
2021-05-13 14:05:24 +0300

    Fixed processing errors

--- backend/choose_word/urls.py
+++ backend/choose_word/urls.py
@@ -6,6 +6,3 @@ from .views import ChooseWordBertView
 urlpatterns = [
     path('bert/', ChooseWordBertView.as_view()),
 ]
-
-handler500 = 'rest_framework.exceptions.server_error'
-handler400 = 'rest_framework.exceptions.bad_request'
--- backend/english_test_solver/urls.py
+++ backend/english_test_solver/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/choose_word/', include('choose_word.urls')),
 ]
+
+handler500 = 'rest_framework.exceptions.server_error'
+handler400 = 'rest_framework.exceptions.bad_request'
