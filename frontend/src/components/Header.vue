5aca8a1c3dff857c1f8218f8629cfe844443abca
ZeRoX <as234zx12@yandex.ru>
2021-05-13 23:42:01 +0300

    Fix text Header and size button

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,11 +1,11 @@
 <template>
   <b-navbar toggleable="lg" type="dark" variant="info">
-    <b-navbar-brand href="#">English Test Solver (logo?)</b-navbar-brand>
+    <b-navbar-brand href="#">English Test Solver</b-navbar-brand>
 
     <b-nav-form class="ml-auto">
       <b-nav-text class="mr-2">The algorithm: </b-nav-text>
       <b-select size="sm" :options="algorithms" v-model="select_algorithm" class="mr-2" />
-      <b-button size="sm" @click="$emit('start')">Start!</b-button>
+      <b-button class="pl-5 pr-5" @click="$emit('start')">Start!</b-button>
     </b-nav-form>
   </b-navbar>
 </template>
