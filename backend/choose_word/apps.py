f3c78c276a336739f1c2fe2fbd2d9dcd40cc957d
Mr.Geekman <bunin260200@mail.ru>
2021-05-23 18:55:07 +0300

    Fixed github pipline

--- backend/choose_word/apps.py
+++ backend/choose_word/apps.py
@@ -15,7 +15,7 @@ class ChooseWordConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'choose_word'
 
-    bert_path = os.path.join(BASE_DIR, 'models', 'BERTBase')
+    bert_path = os.path.join(BASE_DIR, 'models', 'bert-base-uncased')
     bert_tokenizer = BertTokenizer.from_pretrained(bert_path)
     max_bert_size = 512
     max_bert_candidate = 128
@@ -24,7 +24,7 @@ class ChooseWordConfig(AppConfig):
         bert_tokenizer, max_length=max_bert_size
     )
 
-    gpt_path = os.path.join(BASE_DIR, 'models', 'GPTBase')
+    gpt_path = os.path.join(BASE_DIR, 'models', 'gpt2')
     gpt_tokenizer = GPT2Tokenizer.from_pretrained(gpt_path)
     max_gpt_size = 1024
     gpt_scorer_sentence = GPTScorerSentence(
